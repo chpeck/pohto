2836c2200f068ad1d4e179f252f5992c56e011a2
Choon Hong Peck <chpeck@gmail.com>
2011-03-27 23:03:25 -0700

    fix the query

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
     key = params[:album_id].split("-")
-    @photos = Photo.by_date(:startkey=>key.dup.push({}), :endkey=>key,:include_docs=>true, :raw=>true, :descending=>true)["rows"].collect {|row| row["value"] }
+    @photos = Photo.by_date(:startkey=>key.dup.push({}), :endkey=>key, :include_docs=>true, :reduce=>false, :descending=>true)
   end
 end
